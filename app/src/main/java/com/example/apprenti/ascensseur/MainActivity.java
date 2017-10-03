e4a7869431e9160cf2fe595152d297af97ae83a1
ALRUTEN <laurentnavarro31@gmail.com>
2017-10-03 16:56:12 +0200

    ascensseur

---
+++ app/src/main/java/com/example/apprenti/ascensseur/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.apprenti.ascensseur;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isLiftMoving = false;
+    private int currentFloor = 0;
+
+    Button button;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button button10;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(7);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(8);
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(9);
+            }
+        });
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(4);
+            }
+        });
+
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(5);
+            }
+        });
+
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(6);
+            }
+        });
+
+        button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(1);
+            }
+        });
+
+        button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(2);
+            }
+        });
+
+        button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(3);
+            }
+        });
+
+        button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToFloor(0);
+            }
+        });
+    }
+
+    private void goToFloor(int floor) {
+        if (!isLiftMoving && floor != currentFloor) {
+            moveNextFloor(floor);
+            isLiftMoving = false;
+        }
+    }
+
+    private void moveNextFloor(int floor) {
+        if (floor != currentFloor) {
+            isLiftMoving = true;
+            new MoveLift().execute(floor);
+        }
+    }
+
+
+    private class MoveLift extends AsyncTask<Integer, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+            Integer floor = integers[0];
+            SystemClock.sleep(3000);
+            return floor;
+        }
+
+        @Override
+        protected void onPostExecute(Integer floor) {
+            super.onPostExecute(floor);
+            currentFloor = (floor > currentFloor) ? currentFloor + 1 : currentFloor - 1;
+            TextView floorCount = (TextView) findViewById(R.id.floor_count);
+            floorCount.setText(String.valueOf(currentFloor));
+            moveNextFloor(floor);
+            isLiftMoving = false;
+        }
+    }
+
+}
+
+
+
+
